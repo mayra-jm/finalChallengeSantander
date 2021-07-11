ae8c1c423b35641f41c80ee6fc35d52cb4621657
DavidAyalaL <50219055+deividshido@users.noreply.github.com>
2021-07-11 11:23:59 -0500

    Reorganización y carga de evaluación.
    Se reorganizó la estructura de las operaciones establecidas y se agregó la funcionalidad de vista de carga al momento de preparar la evaluación para el registro. Además de incluir una breve descripción sobre la "personalidad" de los usuarios al seleccionar el instrumento que quieren aprender a tocar.

--- src/main/kotlin/s3/main.kt
+++ src/main/kotlin/s3/main.kt
@@ -1,5 +1,7 @@
 package s3
 
+import kotlinx.coroutines.*
+
 var primerAcierto : Int = 0
 var segundoAcierto: Int = 0
 var instrumento: String = ""
@@ -10,12 +12,22 @@ fun main(){
     var disponibilidadTiempo: String
     val week = setOf("LUNES","MARTES", "MIERCOLES",  "JUEVES", "VIERNES" , "SABADO", "DOMINGO" )
 
+    println("Por favor selecciona tu tipo de perfil")
+    println("1) Alumno")
+    println("2) Profesor")
+    var rol= readLine()?.toInt()
+    when (rol){
+        1 -> "Alumno"
+        2 -> "Profesor"
+        else -> println("Selecciona un perfil valido")
+    }
+
     println("¿Cuál es tu nombre?")
     var nombre: String = readLine().toString()
 
     println("Bienvenido $nombre")
 
-    println("¿Qué edad tienes?")
+    println("¿Cuántos años tienes $nombre?")
     var edad: Int = readLine()!!.toInt()
 
     println("¿De qué ciudad eres?")
@@ -26,15 +38,6 @@ fun main(){
         disponibilidadTiempo= readLine().toString().toUpperCase()
     } while (disponibilidadTiempo !in week)
 
-    println("Por favor selecciona tu perfil")
-    println("1) Alumno")
-    println("2) Profesor")
-    var rol= readLine()?.toInt()
-    when (rol){
-        1 -> "Alumno"
-        2 -> "Profesor"
-        else -> println("Selecciona un perfil valido")
-    }
 
     if(rol == 1 ){
         println("¿Haz tocado algún instrumento músical (Si/No)?")
@@ -42,10 +45,74 @@ fun main(){
          instrumento = if (habilidad == "si"){
             println("¿Qué instrumento haz tocado?")
             readLine().toString()
-        } else {
-            println("¿Qué instrumento te gustaría aprender a tocar?")
-            readLine().toString()
+        } else (if (habilidad == "no") {
+             println("¿Qué instrumento te gustaría aprender a tocar?")
+             println("1) Guitarra")
+             println("2) Piano")
+             println("3) Bateria")
+             println("4) Violín")
+             println("5) Bajo")
+             println("6) Otro...")
+
+             var instrumentoUsuario= readLine()?.toInt()
+
+             when (instrumentoUsuario){
+                 1 -> {
+                     println("Interesante, la guitarra suele ser seleccionada por personas intensas y extremadamente sensitivas. \n " +
+                             "Por este motivo, tocar la guitarra puede ayudarte a direccionar todas tus emociones.\n")
+                 } 2 -> {
+                 println("Interesante, el piano no lo suelen tocar las personas extrovertidas. \n" +
+                         "Los pianistas suelen ser individuos con un alto sentido de la estética y la rectitud \n" +
+                         "(Puntuales y muy disciplinados)\n")
+             } 3 -> {
+                 println("Interesante, las personas que tocan batería suelen ser personas seguras de sí mismas. \n" +
+                         "Pero además, acorde a un estudio, los percusionistas tienen un cerebro único, al contar \n" +
+                         "con una especie de cronómetro interno. \n")
+             } 4 -> {
+                 println("Interesante, Debido al tiempo de práctica que requiere, quienes toquen violín deben ser personas \n" +
+                         "constantes y persistentes, que busquen resultados a largo plazo, y no impacientes. \n" +
+                         "Asimismo, dado que se utilizan complicadas técnicas, debe ser alguien extremadamente prolijo y detallista.\n")
+             } 5 -> {
+                 println("Interesante, El bajo suele no ser identificado a primeras por alguna gente, pero es esencial para dar “ambiente” \n" +
+                         "a la canción y unificar el sonido. Las personas que tocan bajo son personas que aman ser “partners”, y que no necesitan \n" +
+                         "todo el tiempo ser el centro de atención, sino que están muy enfocados en los resultados más que en cómo se lucen. \n")
+             } 6 -> {
+                 println("Por favor ingresa el instrumento deseado")
+                 var otroInstrumento = readLine().toString()
+             } else -> {
+                 println("Por favor selecciona una opción valida")
+             }
+             }
+
+         } else {
+             println("Error")
+         }).toString()
+
+        println("A continuación aplicaremos una breve actividad para conocer un poco sobre tus conocimientos en la materia.\n")
+        println("Presiona Enter para continuar.\n")
+        readLine().toString()
+
+        runBlocking {
+            val buyJob = launch(Dispatchers.Default) {
+                var percentage = 0
+
+                println("¿Preparado?\n")
+                println("Presiona Enter para preparar tu evaluación")
+                readLine().toString()
+                println("Afinando intrumentos\n")
+                while (percentage<100){
+                    println("Carga al $percentage%")
+                    delay(400)
+                    percentage+=25
+                }
+                println("¡Evaluación lista!\n")
+            }
         }
+
+        println("Presiona Enter para continuar")
+        readLine().toString()
+
+
        options.play("uno")
         print("¿Desesa reproducirla de nuevo?")
         var again: String = readLine().toString().toUpperCase()
@@ -53,6 +120,7 @@ fun main(){
         if(again == "si"){
             options.reload()
         }
+
         print("¿Qué nota musical escuchas?")
         var notaMusical: String = readLine().toString().toLowerCase()
 
@@ -75,6 +143,7 @@ fun main(){
             println("Excelente!")
         }
     }else if (rol == 2){
+        //Igual que el alumno
         println("Qué instrumento tocas?")
         instrumento  = readLine().toString().toLowerCase()
     }
@@ -84,6 +153,20 @@ fun main(){
 
     val persona = Person("$nombre", edad, "$ciudad", "", "$disponibilidadTiempo")
     if(rol == 1){
+
+        runBlocking {
+            val buyJob = launch(Dispatchers.Default) {
+                var percentage = 0
+
+                while (percentage<100){
+                    println("procesando pago: $percentage%")
+                    delay(500)
+                    percentage+=10
+                }
+                println("Pago exitoso!")
+            }
+        }
+
         val alumnRol = AlumnRol("$habilidad",score, "$instrumento" )
     }else if (rol == 2){
         val professRol = ProfessRol("$instrumento" )
